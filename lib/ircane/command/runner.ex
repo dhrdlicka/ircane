5ae31e908087d6cc64caf2abb357f2298306a086
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2026-05-06 20:44:20 +0200

    fix: swap replies and state in runner return

--- lib/ircane/command/runner.ex
+++ lib/ircane/command/runner.ex
@@ -4,8 +4,8 @@ defmodule IRCane.Command.Runner do
   alias IRCane.Command.Plan
   alias IRCane.User.State, as: UserState
 
-  @spec run(Plan.t()) :: {:ok, UserState.t(), [term()]} | {:error, term()}
-  def run(plan) do
-    {:ok, plan.state, plan.replies}
+  @spec run(Plan.t()) :: {:ok, [term()], UserState.t()} | {:error, term()}
+  def run(%Plan{effects: []} = plan) do
+    {:ok, plan.replies, plan.state}
   end
 end
